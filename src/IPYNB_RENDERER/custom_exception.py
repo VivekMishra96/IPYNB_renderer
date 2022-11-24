f634216284b8cd00643bf42ffe26bfd52d2dc7ff
vivek mishra <starvivek1996@gmail.com>
2022-11-24 19:29:47 +0530

    custom created

--- src/IPYNB_RENDERER/custom_exception.py
+++ src/IPYNB_RENDERER/custom_exception.py
@@ -1,19 +1,4 @@
-import os
-import logging
-import sys
-
-logging_str = "[%(asctime)s: %(levelname)s: %(module)s]: %(message)s"
-log_dir = "./logs"
-log_filepath = os.path.join(log_dir, "running_logs.log")
-os.makedirs(log_dir, exist_ok=True)
-
-logging.basicConfig(
-    level=logging.INFO,
-    format=logging_str,
-    handlers=[
-        logging.FileHandler(log_filepath),
-        logging.StreamHandler(sys.stdout),
-    ],
-)
-
-logger = logging.getLogger("IPYNBrenderer")
+class InvalidURLException(Exception):
+    def __init__(self, message: str = "URL is invalid"):
+        self.message = message
+        super().__init__(self.message)
